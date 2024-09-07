5c3b0d04987869d7a077b9116ccc566367c35288
pedrosenna <pedro_bittencourt@yahoo.com.br>
2024-09-07 15:21:58 -0400

    closes #4; improve hap_collapse

--- R/hap_collapse.R
+++ R/hap_collapse.R
@@ -4,7 +4,7 @@
 #' \code{hap_collapse()} collapses haplotypes from a \code{\link[ape]{DNAbin}} object,
 #' keeping unique haplotypes only.
 #'
-#' @param data an object of class \code{\link[ape]{DNAbin}}.
+#' @param fasta an object of class \code{\link[ape]{DNAbin}}.
 #' @param collapseSubstrings logical. Whether to collapse or not collapse shorter but identical sequences.
 #' @param clean logical. Whether to remove or not remove non ACTG bases from alignment.
 #'
@@ -24,15 +24,49 @@
 #' @author
 #' Rupert A. Collins
 #'
+#' @examples
+#' 
+#' # create a vector of DNA sequences
+#' seqs <- c(seq1= "ATGCTAGTTCGC",
+#'           seq2= "ATGCTAGTTCGC",
+#'           seq3= "ATGCTAGTTCGC",
+#'           seq4= "ATGCTAGTTCG-",
+#'           seq5= "ATGCTAGTT---",
+#'           seq6= "ATGCTAGTTCGN",
+#'           seq7= "ATGCTA-TTCGC",
+#'           seq8= "ATGCTA-TTCG-",
+#'           seq9= "ATGCTANTTCGN",
+#'           seq10="ATGCTAGTT")
+#'           
+#' # convert to DNAbin
+#' dummy <- tolower(seqs) |>
+#'  strsplit(split= "") |>
+#'  ape::as.DNAbin.list()
+#'  
+#' # run hap_collapse with different settings
+#' 
+#' # 2 DNA sequences (seq1/seq7)
+# hap_collapse(dummy, clean = TRUE, collapseSubstrings = TRUE) %>% names()
+
+# 5 DNA sequences (seq1, seq4, seq5, seq7, seq8)
+# hap_collapse(dummy, clean = TRUE, collapseSubstrings = FALSE) %>% names()
+
+# 7 DNA sequences (seq1, seq4, seq5, seq6, seq7, seq8, seq9)
+# hap_collapse(dummy, clean = FALSE, collapseSubstrings = TRUE) %>% names()
+
+# 8 DNA sequences (seq1, seq4, seq5, seq6, seq7, seq8, seq9, seq10)
+# hap_collapse(dummy, clean = FALSE, collapseSubstrings = FALSE) %>% names()           
+#' 
 #' @importFrom stringr str_detect
+#' @importFrom delimtools clean_dna drop_sequences
 #'
 #' @export
-hap_collapse <- function(data, collapseSubstrings = TRUE, clean = TRUE){
+hap_collapse <- function(fasta, collapseSubstrings = TRUE, clean = TRUE){
   if(clean==TRUE){
-    data <- clean_dna(data)
+    data <- delimtools::clean_dna(fasta)
   }
   else{
-    data <- as.list(data)
+    data <- as.list(fasta)
   }
   if(collapseSubstrings==TRUE){
     # sort by length
@@ -43,15 +77,24 @@ hap_collapse <- function(data, collapseSubstrings = TRUE, clean = TRUE){
     data.ord <- mapply(FUN=function(x) paste(x,collapse=""), as.character(data.ord), SIMPLIFY=TRUE, USE.NAMES=FALSE)
     # get the indices of the first match to each seq (the longest)
     ind <- unique(mapply(FUN=function(x) which(stringr::str_detect(string=data.ord, pattern=x) == TRUE)[1], data.ord, SIMPLIFY=TRUE, USE.NAMES=FALSE))
-
-    return(data.ord.copy[ind])
+    # get names of unique haplotypes
+    names_haps <- names(data.ord.copy)[ind]
+    # drop sequences from fasta
+    fasta_haps <- delimtools::drop_sequences(fasta, names_haps, drop = FALSE)
+    
   }
   else{
+    # make a copy to index later
     data.copy <- data
+    # collapse into a strings
     data.char <- mapply(FUN=function(x) paste(x,collapse=""), as.character(data), SIMPLIFY=TRUE, USE.NAMES=FALSE)
+    # get duplicates
     dups <- duplicated(data)
-    data.keep <- data.copy[!dups]
-
-    return(data.keep)
+    # get names of duplicated haplotypes
+    names_dups <- names(data.copy)[dups]
+    # drop sequences from fasta
+    fasta_haps <- delimtools::drop_sequences(fasta, names_dups, drop = TRUE)
+    
   }
+  return(fasta_haps)
 }
