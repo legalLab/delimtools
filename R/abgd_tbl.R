46173eff51741cc2775eab885459c657909c409d
rupert@sorubim <rupertcollins@gmail.com>
2024-09-14 22:32:33 +0100

    webserver option added

--- R/abgd_tbl.R
+++ R/abgd_tbl.R
@@ -16,6 +16,7 @@
 #'   \item 3: simple distance (p-distance)
 #' }
 #' @param outfolder Path to output folder. Default to NULL. If not specified, a temporary location is used.
+#' @param webserver A .txt file containing ABGD results obtained from a webserver. Default to NULL.
 #'
 #' @details
 #' \code{abgd_tbl()} relies on \code{\link[base]{system}} to invoke ABGD software through
@@ -23,6 +24,8 @@
 #' your system in order to use this function properly.
 #' \code{abgd_tbl()} saves all output files in \code{outfolder} and imports the first recursive partition
 #' file generated to \code{Environment}.
+#' Alternatively, \code{abgd_tbl()} can parse a .txt file obtained from a webserver such as 
+#' https://bioinfo.mnhn.fr/abi/public/abgd/abgdweb.html.
 #'
 #' @return
 #' an object of class \code{\link[tibble]{tbl_df}}
@@ -39,13 +42,34 @@
 #' @importFrom glue glue
 #' @importFrom stringr str_replace_all
 #' @importFrom tidyr separate_longer_delim
+#' @importFrom tools file_path_sans_ext
 #'
 #' @export
-abgd_tbl <- function(infile, exe = NULL, haps = NULL, slope = 1.5, model = 3, outfolder = NULL){
+abgd_tbl <- function(infile, exe = NULL, haps = NULL, slope = 1.5, model = 3, outfolder = NULL, webserver = NULL) {
+
+  if(!is.null(webserver) && !file.exists(webserver)) {
+
+    cli::cli_abort("Error: Please provide a valid path to an ABGD results file.")
+
+  }
+
+  if(!is.null(webserver) && file.exists(webserver)) {
+
+    delim <- readr::read_delim(webserver, delim = ";", col_names = c("abgd", "labels"), col_types = "c")
+    
+    delim <- delim |> 
+      dplyr::mutate(abgd = 1:nrow(delim)) |> 
+      dplyr::mutate(labels = stringr::str_replace_all(labels, "id: " , "")) |> 
+      tidyr::separate_longer_delim(cols = labels,delim = " ") |> 
+      dplyr::relocate(labels, .before = abgd)
+
+    return(delim)
+
+  }
 
   if(!file.exists(exe)){
 
-    cli::cli_abort("Error: Please provide a valid path to the ASAP executable file.")
+    cli::cli_abort("Error: Please provide a valid path to the ABGD executable file.")
   
   }
 
@@ -74,7 +98,9 @@ abgd_tbl <- function(infile, exe = NULL, haps = NULL, slope = 1.5, model = 3, ou
 
   res <- system(command=string_abgd, intern = TRUE)
 
-  fpath <- glue::glue('{outfolder}/{stringr::str_split_fixed(basename(infile), "\\\\.", 2)[,1]}.part.1.txt')
+  #fpath <- glue::glue('{outfolder}/{stringr::str_split_fixed(basename(infile), "\\\\.", 2)[,1]}.part.1.txt')
+  fpath <- glue::glue('{outfolder}/{tools::file_path_sans_ext(basename(infile))}.part.1.txt')
+
 
   delim <- readr::read_delim(fpath, delim = ";", col_names = c("abgd", "labels"), col_types = "c")
     
