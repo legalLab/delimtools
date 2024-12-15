d6071d1bfeb73fc9f6f118ef6909bef8a2da41a1
pedrosenna <pedro_bittencourt@yahoo.com.br>
2024-12-15 16:13:32 -0400

    rename argument

--- R/collapse_others.R
+++ R/collapse_others.R
@@ -34,15 +34,15 @@ collapse_others <- function(data, hap_tbl, identifier, cols){
                              ~{ hap_tbl %>%
                                  dplyr::mutate(collapsed= stringr::str_split(collapsed, ", ")) %>%
                                  tidyr::unnest(c(collapsed, n_seqs)) %>%
-                                 dplyr::mutate(x= dplyr::pull(data, .x)[match(haplotype, dplyr::pull(data, {{ identifier }}))],
+                                 dplyr::mutate(x= dplyr::pull(data, .x)[match(labels, dplyr::pull(data, {{ identifier }}))],
                                                y= dplyr::pull(data, .x)[match(collapsed, dplyr::pull(data, {{ identifier }}))]) %>%
                                  dplyr::mutate("collapsed_{.x}" := dplyr::if_else(is.na(y),
                                                                                   x,
-                                                                                  paste(unique(c(x, y)), collapse = ", ")), .by = haplotype) %>%
-                                 dplyr::select(c(haplotype, n_seqs, tidyselect::ends_with(cols))) %>%
-                                 dplyr::distinct(haplotype, .keep_all = TRUE) %>%
-                                 dplyr::full_join(hap_tbl, ., by= c("haplotype", "n_seqs"))}) %>%
-    purrr::reduce(., dplyr::full_join, by= c("haplotype", "n_seqs", "collapsed"))
+                                                                                  paste(unique(c(x, y)), collapse = ", ")), .by = labels) %>%
+                                 dplyr::select(c(labels, n_seqs, tidyselect::ends_with(cols))) %>%
+                                 dplyr::distinct(labels, .keep_all = TRUE) %>%
+                                 dplyr::full_join(hap_tbl, ., by= c("labels", "n_seqs"))}) %>%
+    purrr::reduce(., dplyr::full_join, by= c("labels", "n_seqs", "collapsed"))
 
   return(collapse_tbl)
 }
